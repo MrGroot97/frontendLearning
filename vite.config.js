0ca31b8a584effc508718864aa61f1949ac65bd3
mrgroot16 <2016281@iiitdmj.ac.in>
2024-07-28 00:57:51 +0530

    add: added github actions yml files

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/js_interview_prep/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef

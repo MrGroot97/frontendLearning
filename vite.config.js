da1072af4445eef126f46457df0c915910e25c09
mrgroot16 <2016281@iiitdmj.ac.in>
2024-07-28 01:49:39 +0530

    fix: removed github pages files

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/js_interview_prep/",
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef
